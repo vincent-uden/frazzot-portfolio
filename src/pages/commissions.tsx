d2cd0765afdbc52e4a11c63f5395b8bc8521a0c4
Vincent Uden <vincentuden@gmail.com>
2022-08-16 03:01:32 +0200

    Added fields for commission form

--- src/pages/commissions.tsx
+++ src/pages/commissions.tsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const Comissions = () => {
   return (
     <>
       <div className="w-screen bg-pattern-holo-short-inv bg-[length:1920px_330px] bg-repeat-x overflow-y-hidden">
         <div className="h-64"></div>
-        <h1 className='font-stretch text-center text-periwinkle text-6xl pl-4'>
+        <h1 className="font-stretch text-center text-periwinkle text-6xl pl-4">
           <span>COM</span> <span className="relative -left-8">MISSIONS</span>
         </h1>
         <div className="bg-holo bg-cover mt-8 py-2">
-          <h2 className='font-stretch text-center text-greyblack text-3xl'>INFORMATION AND FORM_</h2>
+          <h2 className="font-stretch text-center text-greyblack text-3xl">
+            INFORMATION AND FORM_
+          </h2>
         </div>
       </div>
 
@@ -26,23 +28,132 @@ const Comissions = () => {
           </div>
           <div className="inline-block w-[5%]"></div>
           <div className="inline-block w-[55%] border-2 border-white p-6">
-            <p className="text-periwinkle-light font-cocogoose font-thin text-lg">IN MY PATREON POST BELOW YOU WILL FIND ALL THE INFORMATION NEEDED IN ORDER TO COMMISSION ME.</p>
+            <p className="text-periwinkle-light font-cocogoose font-thin text-lg">
+              IN MY PATREON POST BELOW YOU WILL FIND ALL THE INFORMATION NEEDED
+              IN ORDER TO COMMISSION ME.
+            </p>
             <div className="h-6"></div>
-            <p className="text-periwinkle-light font-cocogoose font-thin text-lg">TO THE LEFT YOU WILL FIND MY TERMS AND COMMISSIONS FOR USING MY SERVICES. PLEASE READ IT CAREFULLY BEFORE FILLING OUT THE FORM</p>
+            <p className="text-periwinkle-light font-cocogoose font-thin text-lg">
+              TO THE LEFT YOU WILL FIND MY TERMS AND COMMISSIONS FOR USING MY
+              SERVICES. PLEASE READ IT CAREFULLY BEFORE FILLING OUT THE FORM
+            </p>
           </div>
         </div>
         <div className="h-12"></div>
         <div className="bg-mint w-full p-8">
-          <h2 className="font-stretch text-xl text-greyblack text-center">READ ABOUT MY <span>COM</span> <span className="relative -left-3">MISSIONS HERE</span></h2>
+          <h2 className="font-stretch text-xl text-greyblack text-center">
+            READ ABOUT MY <span>COM</span>{" "}
+            <span className="relative -left-3">MISSIONS HERE</span>
+          </h2>
+        </div>
+        <div className="h-8"></div>
+
+        <div className="px-[20%]">
+          <h2 className="font-stretch text-2xl text-pastelpink mb-4 mt-24">
+            GENERAL INFO
+          </h2>
+          <label htmlFor="" className="label mt-8">
+            Name
+          </label>
+          <input
+            className="text-input focus:border-lilac transition-colors"
+            type="text"
+            name="name"
+            id="name"
+          />
+          <label htmlFor="" className="label mt-8">
+            Email
+          </label>
+          <input
+            className="text-input focus:border-lilac transition-colors"
+            type="text"
+            name="name"
+            id="name"
+          />
+          <label htmlFor="" className="label mt-8">
+            Type/Category of commission
+          </label>
+          <input
+            className="text-input focus:border-lilac transition-colors"
+            type="text"
+            name="name"
+            id="name"
+          />
+          <label htmlFor="" className="label mt-8">
+            How many characters
+          </label>
+          <input
+            className="text-input focus:border-lilac transition-colors"
+            type="text"
+            name="name"
+            id="name"
+          />
+          <label htmlFor="" className="label mt-8">
+            Any specific wishes or details?
+          </label>
+          <input
+            className="text-input focus:border-lilac transition-colors"
+            type="text"
+            name="name"
+            id="name"
+          />
+          <label htmlFor="" className="label mt-8">
+            Background?
+          </label>
+          <input
+            className="text-input focus:border-lilac transition-colors"
+            type="text"
+            name="name"
+            id="name"
+          />
+
+          <h2 className="font-stretch text-2xl text-mint mt-12 mb-4">
+            CHARACTER(S) INFO
+          </h2>
+          <label htmlFor="" className="label mt-8 text-mint">
+            Character(s) name(s)
+          </label>
+          <input
+            className="text-input focus:border-lilac transition-colors text-mint border-mint"
+            type="text"
+            name="name"
+            id="name"
+          />
+          <label htmlFor="" className="label mt-8 text-mint">
+            Character(s) personality/description
+          </label>
+          <input
+            className="text-input focus:border-lilac transition-colors text-mint border-mint"
+            type="text"
+            name="name"
+            id="name"
+          />
+
+          <h2 className="font-stretch text-2xl text-sky no-ligature mt-12 mb-4">
+            Additional info
+          </h2>
+          <p className="font-cocogoose font-thin text-sky text-lg mb-4">
+            This is your place for questions, clarifications and additional
+            information you want me to know. If you want to commission a
+            character sheet, you can provide the elements you want to include in
+            the sheet here. (e.g. stats, bio, expressions, weapon, item,
+            palette, icons, etc.)
+          </p>
+          <textarea
+            className="text-input border-2 p-2 border-sky resize-none text-base text-sky placeholder:text-sky placeholder:opacity-60 focus:border-lilac transition-colors"
+            name="additional-info"
+            id="additional-info"
+            rows={10}
+            maxLength={2500}
+          ></textarea>
+          <button className="block bg-yellowpeach font-stretch text-2xl text-greyblack py-4 border-2 border-yellowpeach hover:bg-greyblack hover:text-yellowpeach transition-colors w-full mt-12">
+            SUBMIT
+          </button>
         </div>
         <div className="h-64"></div>
       </div>
-
     </>
-  )
+  );
 };
 
-
-
-
-export default Comissions
+export default Comissions;
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,13 +1,13 @@
-import Link from 'next/link';
-import React, { useState } from 'react'
-import { trpc } from '../utils/trpc';
-import { EmailError } from '../utils/errortypes';
+import Link from "next/link";
+import React, { useState } from "react";
+import { trpc } from "../utils/trpc";
+import { EmailError } from "../utils/errortypes";
 
 const Contact = () => {
   const [name, setName] = useState<string>("");
-  const [email, setEmail] = useState<string>("")
-  const [subject, setSubject] = useState<string>("")
-  const [errors, setErrors] = useState<EmailError[]>([])
+  const [email, setEmail] = useState<string>("");
+  const [subject, setSubject] = useState<string>("");
+  const [errors, setErrors] = useState<EmailError[]>([]);
 
   const submitFormMut = trpc.useMutation(["email.submitContact"], {
     onSuccess: (data) => {
@@ -16,14 +16,21 @@ const Contact = () => {
       let nameLabel = document.getElementById("nameDiv");
       if (data.errors.includes(EmailError.EmptyName)) {
         nameLabel?.classList.toggle("shake-activate");
-        setTimeout(() => { nameLabel?.classList.toggle("shake-activate") }, 100);
+        setTimeout(() => {
+          nameLabel?.classList.toggle("shake-activate");
+        }, 100);
       }
 
       setTimeout(() => {
         let emailLabel = document.getElementById("emailDiv");
-        if (data.errors.includes(EmailError.EmptyEmail) || data.errors.includes(EmailError.InvalidEmail)) {
+        if (
+          data.errors.includes(EmailError.EmptyEmail) ||
+          data.errors.includes(EmailError.InvalidEmail)
+        ) {
           emailLabel?.classList.toggle("shake-activate");
-          setTimeout(() => { emailLabel?.classList.toggle("shake-activate") }, 100);
+          setTimeout(() => {
+            emailLabel?.classList.toggle("shake-activate");
+          }, 100);
         }
       }, 100);
 
@@ -31,42 +38,64 @@ const Contact = () => {
         let messageLabel = document.getElementById("msgDiv");
         if (data.errors.includes(EmailError.EmptyMessage)) {
           messageLabel?.classList.toggle("shake-activate");
-          setTimeout(() => { messageLabel?.classList.toggle("shake-activate") }, 100);
+          setTimeout(() => {
+            messageLabel?.classList.toggle("shake-activate");
+          }, 100);
         }
       }, 200);
     },
-  })
+  });
 
   return (
     <>
       <div className="w-screen bg-pattern-holo-short-inv bg-[length:1920px_330px] bg-repeat-x overflow-y-hidden">
         <div className="h-64"></div>
-        <h1 className='font-stretch text-center text-pastelpink text-6xl pl-4'>
+        <h1 className="font-stretch text-center text-pastelpink text-6xl pl-4">
           CONTACT
         </h1>
         <div className="bg-holo bg-cover mt-8 mb-16 py-2">
-          <h2 className='font-stretch text-center text-greyblack text-3xl'><span>HOW TO CONTACT AND CON</span> <span className='relative -left-4'>NECT WITH ME_</span></h2>
+          <h2 className="font-stretch text-center text-greyblack text-3xl">
+            <span>HOW TO CONTACT AND CON</span>{" "}
+            <span className="relative -left-4">NECT WITH ME_</span>
+          </h2>
         </div>
       </div>
       <div className="flex flex-row px-48">
         <aside>
           <div className="border-2 p-12 border-lilac">
-            <h2 className="font-stretch text-2xl text-pastelpink no-ligature">COLLABORATION ?</h2>
-            <div className="h-8"></div>
-            <p className="font-cocogoose font-thin text-pastelpink text-lg">IF YOU WISH TO COLLABORATE WITH ME. REACH OUT TO ME THROUGH EMAIL OR DISCORD</p>
-          </div>
-          <div className="border-2 p-12 my-8 border-sky">
-            <h2 className="font-stretch text-2xl text-pastelpink no-ligature">ORDER PHYSICAL COPIES ?</h2>
-            <div className="h-8"></div>
-            <p className="font-cocogoose font-thin text-pastelpink text-lg">SINCE I HAVE NO STORE, THERE ARE NO SET PRICES ON MY PHYSICAL COPIES. YOU CAN REACH OUT TO ME THROUGH EMAIL IF YOU'RE INTERESTED.</p>
-            <div className="h-4"></div>
-            <p className="font-cocogoose font-thin text-pastelpink text-lg">IF YOU'RE INTERESTED IN ORDERING OTHER PRODUCTS I CREATE LIKE PAINTED CLOTHING OR PAINTINGS, REACH OUT TO ME THROUGH EMAIL.</p>
-          </div>
-          <div className="border-2 p-12 border-mint">
-            <h2 className="font-stretch text-2xl text-pastelpink"><span className='no-ligature'>COMM</span>ISSION ?</h2>
+            <h2 className="font-stretch text-2xl text-pastelpink no-ligature">
+              COLLABORATION ?
+            </h2>
             <div className="h-8"></div>
             <p className="font-cocogoose font-thin text-pastelpink text-lg">
-              READ MORE ABOUT MY COMMISSIONS ON THE <Link href="/commissions">
+              IF YOU WISH TO COLLABORATE WITH ME. REACH OUT TO ME THROUGH EMAIL
+              OR DISCORD
+            </p>
+          </div>
+          <div className="border-2 p-12 my-8 border-sky">
+            <h2 className="font-stretch text-2xl text-pastelpink no-ligature">
+              ORDER PHYSICAL COPIES ?
+            </h2>
+            <div className="h-8"></div>
+            <p className="font-cocogoose font-thin text-pastelpink text-lg">
+              SINCE I HAVE NO STORE, THERE ARE NO SET PRICES ON MY PHYSICAL
+              COPIES. YOU CAN REACH OUT TO ME THROUGH EMAIL IF YOU'RE
+              INTERESTED.
+            </p>
+            <div className="h-4"></div>
+            <p className="font-cocogoose font-thin text-pastelpink text-lg">
+              IF YOU'RE INTERESTED IN ORDERING OTHER PRODUCTS I CREATE LIKE
+              PAINTED CLOTHING OR PAINTINGS, REACH OUT TO ME THROUGH EMAIL.
+            </p>
+          </div>
+          <div className="border-2 p-12 border-mint">
+            <h2 className="font-stretch text-2xl text-pastelpink">
+              <span className="no-ligature">COMM</span>ISSION ?
+            </h2>
+            <div className="h-8"></div>
+            <p className="font-cocogoose font-thin text-pastelpink text-lg">
+              READ MORE ABOUT MY COMMISSIONS ON THE{" "}
+              <Link href="/commissions">
                 <span className="font-cocogoose font-normal cursor-pointer">
                   COMMISSION PAGE.
                 </span>
@@ -76,7 +105,9 @@ const Contact = () => {
         </aside>
         <div className="bg-holo w-8 mx-8"></div>
         <aside className="flex flex-col">
-          <h2 className="font-stretch text-2xl text-pastelpink w-[20em]">EMAIL FORM</h2>
+          <h2 className="font-stretch text-2xl text-pastelpink w-[20em]">
+            EMAIL FORM
+          </h2>
           <div className="h-8"></div>
           <div className="flex flex-col justify-between flex-grow">
             <div>
@@ -85,34 +116,75 @@ const Contact = () => {
                   NAME
                 </label>
                 <p className="float-right label text-base opacity-60">
-                  {errors.includes(EmailError.EmptyName) ? "Can't be empty" : ""}
+                  {errors.includes(EmailError.EmptyName)
+                    ? "Can't be empty"
+                    : ""}
                 </p>
               </div>
-              <input className="text-input focus:border-lilac transition-colors" type="text" name="" id="name" value={name} onChange={(e) => setName(e.target.value)} />
+              <input
+                className="text-input focus:border-lilac transition-colors"
+                type="text"
+                name=""
+                id="name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
             </div>
             <div>
               <div className="angry-shake" id="emailDiv">
                 <label htmlFor="email" className="label float-left" id="email">
                   {"EMAIL ADDRESS"}
                 </label>
-                <p className='float-right label text-base opacity-60'>
-                  {(errors.includes(EmailError.EmptyEmail) ? " Can't be empty" : "") + (errors.includes(EmailError.InvalidEmail) ? " Invalid email address" : "")}
+                <p className="float-right label text-base opacity-60">
+                  {(errors.includes(EmailError.EmptyEmail)
+                    ? " Can't be empty"
+                    : "") +
+                    (errors.includes(EmailError.InvalidEmail)
+                      ? " Invalid email address"
+                      : "")}
                 </p>
               </div>
-              <input className="text-input focus:border-lilac transition-colors" type="email" name="" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+              <input
+                className="text-input focus:border-lilac transition-colors"
+                type="email"
+                name=""
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </div>
             <div>
               <div className="angry-shake" id="msgDiv">
-                <label htmlFor="subject" className="label float-left" id="message">
+                <label
+                  htmlFor="subject"
+                  className="label float-left"
+                  id="message"
+                >
                   SUBJECT
                 </label>
                 <p className="float-right label text-base opacity-60">
-                  {(errors.includes(EmailError.EmptyMessage) ? "Can't be empty" : "")}
+                  {errors.includes(EmailError.EmptyMessage)
+                    ? "Can't be empty"
+                    : ""}
                 </p>
               </div>
-              <textarea className="text-input border-2 p-2 resize-none text-base placeholder:text-pastelpink placeholder:opacity-60 focus:border-lilac transition-colors" name="" id="subject" rows={8} maxLength={500} value={subject} onChange={(e) => setSubject(e.target.value)} placeholder="Interested in commissions? Fill out the form on the commission page" />
+              <textarea
+                className="text-input border-2 p-2 resize-none text-base placeholder:text-pastelpink placeholder:opacity-60 focus:border-lilac transition-colors"
+                name=""
+                id="subject"
+                rows={8}
+                maxLength={500}
+                value={subject}
+                onChange={(e) => setSubject(e.target.value)}
+                placeholder="Interested in commissions? Fill out the form on the commission page"
+              />
             </div>
-            <button className="block bg-pastelpink font-stretch text-2xl text-greyblack py-4 border-2 border-pastelpink hover:bg-greyblack hover:text-pastelpink transition-colors" onClick={(e) => submitFormMut.mutate({ name, email, subject })}>SUBMIT</button>
+            <button
+              className="block bg-pastelpink font-stretch text-2xl text-greyblack py-4 border-2 border-pastelpink hover:bg-greyblack hover:text-pastelpink transition-colors"
+              onClick={(e) => submitFormMut.mutate({ name, email, subject })}
+            >
+              SUBMIT
+            </button>
           </div>
         </aside>
       </div>
@@ -122,7 +194,7 @@ const Contact = () => {
           <img src="/DiscordBrand.svg" alt="" />
         </aside>
         <aside className="bg-holo bg-cover flex-grow relative shadow-left">
-          <h2 className='absolute w-full font-stretch text-center text-greyblack text-3xl no-ligature top-1/2 -translate-y-1/2 transition-transform hover:scale-110 cursor-pointer py-4'>
+          <h2 className="absolute w-full font-stretch text-center text-greyblack text-3xl no-ligature top-1/2 -translate-y-1/2 transition-transform hover:scale-110 cursor-pointer py-4">
             <Link href={"https://discord.gg/MAQm86a3Xw"}>
               JOIN MY COMMUNITY HERE &gt;
             </Link>
@@ -130,10 +202,9 @@ const Contact = () => {
         </aside>
       </div>
 
-      <div className="w-screen px-[20%] h-72 pt-12 bg-pattern-holo-short bg-[length:1920px_330px] bg-repeat-x bg-bottom overflow-y-hidden">
-      </div>
+      <div className="w-screen px-[20%] h-72 pt-12 bg-pattern-holo-short bg-[length:1920px_330px] bg-repeat-x bg-bottom overflow-y-hidden"></div>
     </>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
