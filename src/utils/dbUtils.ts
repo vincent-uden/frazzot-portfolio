1914955d456f18c90a50c78b10e19d9151e1e932
Vincent Udén <vincent.uden@colmeo.se>
2022-10-04 15:12:05 +0200

    Switch from ts-node to tsno which actually works

--- src/utils/dbUtils.ts
+++ src/utils/dbUtils.ts
@@ -1,5 +1,6 @@
+// Run with "npx tsno run src/utils/dbUtils.ts"
+
 import { prisma } from "../server/db/client";
-import * as argon2 from "argon2";
 
 const args = require("args-parser")(process.argv);
 console.info(args);
